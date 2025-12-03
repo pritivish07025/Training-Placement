e76383856ce76b7a3864351e96ca83d2a7ecba01
Priti Vishwakarma <priti.vishwakarma311664@gmail.com>
2025-12-04 01:46:27 +0530

    commit

---
+++ Day-7/1.c
@@ -0,0 +1,13 @@
+// impliment stack using array
+
+#include <stdio.h>
+int main(){
+    int size;
+    printf("enter size");
+    scanf("%d",&size);
+    int arr[size];
+    for(int i=0;i<size;i++){
+        printf("Print array",arr[i]);  
+    }
+    
+}
